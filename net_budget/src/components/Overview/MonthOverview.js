d9e4564fdb87a702ed24fe2f4cc8b926448ef41e
Jose Mendoza <mendozaj9486@gmail.com>
2022-12-07 13:37:51 -0500

    Added functionality for adding pTransaction

--- net_budget/src/components/Overview/MonthOverview.js
+++ net_budget/src/components/Overview/MonthOverview.js
@@ -5,8 +5,8 @@ const MonthOverview = (props) => {
     const { monthIndex } = props;
     const { monthOverview } = useSelector((state) => state.transaction);
 
-    const potNet = monthOverview[monthIndex].potentialNet;
-    const proNet = monthOverview[monthIndex].projectedNet;
+    const potNet = monthOverview[monthIndex].potNet;
+    const projNet = monthOverview[monthIndex].projNet;
     const netTotal = monthOverview[monthIndex].net;
 
     const textClass = (amount) => amount < 0 ? 'negative' : '';
@@ -16,7 +16,7 @@ const MonthOverview = (props) => {
             <li className='tooltip'>
                 <h4>Potential Net</h4>
                 <p className={textClass(potNet)}>{'$' + potNet.toFixed(2)}</p>
-                <span className='tooltiptext tooltip-left arrow-right'>Potential Net is calculated by subtracting potential transactions from your current net. (Net - Potential Transactions)</span>
+                <span className='tooltiptext tooltip-left arrow-right'>Potential Net is calculated by subtracting potential transactions from your current net. (Net + Potential Transactions)</span>
             </li>
             <li className='middle'>
                 <h4>Net</h4>
@@ -24,7 +24,7 @@ const MonthOverview = (props) => {
             </li>
             <li className='tooltip'>
                 <h4>Projected Net</h4>
-                <p className={textClass(proNet)}>{'$' + proNet.toFixed(2)}</p>
+                <p className={textClass(projNet)}>{'$' + projNet.toFixed(2)}</p>
                 <span className='tooltiptext tooltip-right arrow-left'>Projected Net is calculated by adding your projected income to your potential net. (Potential Net + Projected Income)</span>
             </li>
         </ul >
