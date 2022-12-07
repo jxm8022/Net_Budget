6f6e3ae88931af333378f9c02a68c26da2f66d8c
Jose Mendoza <mendozaj9486@gmail.com>
2022-12-07 09:03:47 -0500

    Updated reducer to remove state mutation

--- net_budget/src/reducers/transactionReducer.js
+++ net_budget/src/reducers/transactionReducer.js
@@ -2,7 +2,7 @@ import * as types from '../actions/actionTypes';
 
 const initialState = {
     startYear: new Date().getFullYear(),
-    currentYear: new Date().getFullYear() + 1,
+    currentYear: new Date().getFullYear(),
     previousYear: null,
     monthOverview: [
         { potentialNet: 0, potentialIncome: 0, projectedNet: 0, net: 0, transactions: [], potentialTransactions: [], projectedIncome: [] },
@@ -24,12 +24,17 @@ const transactionReducer = (state = initialState, action) => {
     switch (action.type) {
         case types.ADD_TRANSACTION:
             if (action.payload.type <= 4) { // want, need, savings, debt
-                console.log('want, need, savings, debt')
-                console.log(parseInt(action.payload.date.slice('-')[1]));
-                console.log([...state.monthOverview][action.payload.date.slice('-')[1]].net)
-                return {
+                const monthIndex = parseInt(action.payload.date.split('-')[1]) - 1;
+                const previousNet = state.monthOverview[monthIndex].net;
+                const updatedMonth = { ...state.monthOverview[monthIndex], net: previousNet - action.payload.amount, transactions: [...state.monthOverview[monthIndex].transactions, action.payload] };
+                let newMonthOverview = [...state.monthOverview];
+                newMonthOverview[monthIndex] = updatedMonth;
+
+                let newState = {
                     ...state,
-                }
+                    monthOverview: newMonthOverview
+                };
+                return newState;
             }
             return { ...state };
         default:
