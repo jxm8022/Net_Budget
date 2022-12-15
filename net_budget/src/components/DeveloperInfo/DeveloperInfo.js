656242a398a7bb038810b241b62fb8e1f745d91d
Jose Mendoza <mendozaj9486@gmail.com>
2022-12-15 14:35:31 -0500

    Added images; Updated about page

--- net_budget/src/components/DeveloperInfo/DeveloperInfo.js
+++ net_budget/src/components/DeveloperInfo/DeveloperInfo.js
@@ -1,15 +1,25 @@
+import github from '../../assets/images/socialMedia/github-48.png';
+import linkedin from '../../assets/images/socialMedia/linkedin-48.png';
+import me from '../../assets/images/socialMedia/me.jpeg';
 import './DeveloperInfo.css';
 
 const DeveloperInfo = () => {
     return (
         <>
-            <svg id='developer' height={200} width={200}>
-                <circle cx={100} cy={100} r="100" fill='grey'></circle>
-            </svg>
+            <hr id='developer' className='about-separator' color='black'></hr>
+            <img src={me} alt='Developer Avatar' style={{ 'border-radius': '50%' }} />
             <h2>Jose Mendoza</h2>
-            <p>hello</p>
-            <p>more info</p>
-            <p>link github</p>
+            <p className='bio'><b>Bio:</b>
+                <p>Hobbies: programming, gaming, stargazing, 3D printing</p>
+                <p>Interests: astronomy, mathematics, science</p>
+            </p>
+            <p className='background'><b>Background:</b>
+                <p>Education: The University of Texas at Arlington</p>
+            </p>
+            <ul className='social-media'>
+                <li><a rel='noreferrer noopener' target='_blank' href='https://github.com/jxm8022'><img src={github} alt='GitHub logo.' /></a></li>
+                <li><a rel='noreferrer noopener' target='_blank' href='https://www.linkedin.com/in/josemendoza9486/'><img src={linkedin} alt='Linkedin logo.' /></a></li>
+            </ul>
         </>
     );
 }
--- net_budget/src/components/SiteInfo/SiteInfo.js
+++ net_budget/src/components/SiteInfo/SiteInfo.js
@@ -1,12 +1,31 @@
+import { useSelector } from 'react-redux';
 import './SiteInfo.css';
 
 const Definitions = () => {
     return (
         <>
             <h2 id='definitions'>Definitions</h2>
-            <p><b>Net: </b>The amount of money available to spend in the corresponding month based off of month net after potential transactions are factored.</p>
-            <p><b>Potential Net: </b>The amount of money available to spend in the corresponding month based off of month net after potential transactions are factored.</p>
-            <p><b>Projected Net: </b>The amount of money available to spend in the corresponding month based off of month net after potential transactions are factored.</p>
+            <div className='about-body'>
+                <p><b>Net: </b>The amount of money available to spend in the corresponding month based off of month net after potential transactions are factored.</p>
+                <p className='equation'>Net = Income - Transactions</p>
+                <p><b>Potential Net: </b>The amount of money available to spend in the corresponding month based off of month net after potential transactions are factored.</p>
+                <p className='equation'>Potential Net = Net - Potential Transactions</p>
+                <p><b>Projected Income: </b>The amount of money that will be potentially available during the month.</p>
+                <p><b>Projected Net: </b>The amount of money available to spend in the corresponding month based off of month net after potential transactions are factored.</p>
+                <p className='equation'>Projected Net = Net - Projected Income</p>
+                <p><b>Want: </b>Transactions that are not crucial.</p>
+                <p className='equation'>Examples: movies, fast food, snacks, etc</p>
+                <p><b>Need: </b>Transactions that are necessary for daily life.</p>
+                <p className='equation'>Examples: bills, groceries, gas, etc</p>
+                <p><b>Savings: </b>Money set aside to not be spent.</p>
+                <p className='equation'>Example: transfer from debit account to savings account.</p>
+                <p><b>Debt: </b>Money put towards paying off a debt.</p>
+                <p className='equation'>Examples: credit cards, loans, etc</p>
+                <p><b>Income: </b>Money received from any medium.</p>
+                <p className='equation'>Examples: paycheck, zelle, etc</p>
+                <p><b>pTransaction: </b>Potential transactions that have not happened yet that are of any type, besides income.</p>
+                <p><b>pIncome: </b>Potential incomes that have not happened yet.</p>
+            </div>
         </>
     );
 }
@@ -15,17 +34,28 @@ const HowTo = () => {
     return (
         <>
             <h2 id='howto'>How To</h2>
-            <p>Best way is to...</p>
+            <p className='about-body'>
+                Based off of your personal bank records, <a href='./addTransaction' className='inline-anchor'>add a transaction</a> when a payment has be finalized on your account.
+                The transaction type determines how your budget is calculated. For pending transactions, they have their own types, <b>pTransaction</b> and <b>pIncome</b>.
+            </p>
         </>
     );
 }
 
 const SiteInfo = () => {
+    const { currentMonth, currentYear } = useSelector((state) => state.transaction);
     return (
         <>
-            <a href='#howto'>How To</a>
-            <a href='#definitions'>Definitions</a>
-            <a href='#developer'>About Developer</a>
+            <ul className='page-references'>
+                <li><a href='#howto'>How To</a></li>
+                <li className='middle'><a href='#developer'>About Developer</a></li>
+                <li><a href='#definitions'>Definitions</a></li>
+            </ul >
+            <p className='about-body'>
+                The <a href='.' className='inline-anchor'>bar chart</a> displays the net information for each month in a given year.
+                The <a href={`./monthOverview?month=${currentMonth}&year=${currentYear}`} className='inline-anchor'>month overview</a> shows how you are doing in a given month.
+                The month overview has <b>Potential Net</b>, <b>Net</b>, <b>Projected Net</b>, and a table with corresponding transactions.
+            </p>
             <HowTo />
             <Definitions />
         </>
