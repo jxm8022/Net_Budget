f6dfd47420b990ee75c1426c6d0398b6315adee3
Jose Mendoza <mendozaj9486@gmail.com>
2022-12-20 11:12:50 -0500

    Added table type to use same component for ratios

--- net_budget/src/components/UI/Table/Table.js
+++ net_budget/src/components/UI/Table/Table.js
@@ -6,7 +6,7 @@ import Modal from '../Modal/Modal';
 import './Table.css';
 
 const Table = (props) => {
-    const { headers, data, isSortAsc, sortColumn, sortTable } = props;
+    const { headers, dataType, data, isSortAsc, sortColumn, sortTable, hideTable } = props;
     const [showModal, setShowModal] = useState();
 
     let sortImage = <img src={sortAsc} alt='Sorting Ascending' />;
@@ -22,26 +22,52 @@ const Table = (props) => {
         setShowModal(null);
     }
 
+    let table;
+
+    if (dataType === 'TRANSACTIONS') {
+        table = <table className="table">
+            <thead>
+                <tr>
+                    {headers.map((header) => <th key={header} onClick={() => sortTable(header)}>{header}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {data.length > 0 ? data.map((item) => (
+                    <tr key={item.id} onClick={() => updateRow(item)}>
+                        <td>{categories[item.type].type}</td>
+                        <td>{item.date}</td>
+                        <td>{item.name}</td>
+                        <td>${item.amount.toFixed(2)}</td>
+                    </tr>
+                )) : <tr style={{ height: '48px' }}><td> </td><td> </td><td> </td><td> </td></tr>}
+            </tbody>
+        </table>;
+    }
+
+    const textClass = (amount) => amount < 0 ? 'negative' : '';
+    if (dataType === 'RATIOS') {
+        table = <table className="table" onClick={hideTable}>
+            <thead>
+                <tr>
+                    {headers.map((header) => <th key={header}>{header}{sortColumn === header && sortImage}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {data.length > 0 ? data.map((item) => (
+                    <tr key={item.id}>
+                        <td>{item.type}</td>
+                        <td>${item.available.toFixed(2)}</td>
+                        <td className={textClass(item.remaining)}>${item.remaining.toFixed(2)}</td>
+                    </tr>
+                )) : <tr style={{ height: '48px' }}><td> </td><td> </td><td> </td></tr>}
+            </tbody>
+        </table>;
+    }
+
     return (
         <>
             {showModal && <Modal data={showModal} closeModal={closeModal} />}
-            <table className="table">
-                <thead>
-                    <tr>
-                        {headers.map((header) => <th key={header} onClick={() => sortTable(header)}>{header}{sortColumn === header && sortImage}</th>)}
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.length > 0 ? data.map((item) => (
-                        <tr key={item.id} onClick={() => updateRow(item)}>
-                            <td>{categories[item.type].type}</td>
-                            <td>{item.date}</td>
-                            <td>{item.name}</td>
-                            <td>${item.amount.toFixed(2)}</td>
-                        </tr>
-                    )) : <tr style={{ height: '48px' }}><td> </td><td> </td><td> </td><td> </td></tr>}
-                </tbody>
-            </table>
+            {table}
         </>
     );
 }
