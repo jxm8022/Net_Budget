a5cd0c56ee7100870567043100101ff910dbea9f
Jose Mendoza <mendozaj9486@gmail.com>
2022-12-29 11:41:14 -0600

    Removed unnecessary import

--- net_budget/src/components/Auth/AuthForm.js
+++ net_budget/src/components/Auth/AuthForm.js
@@ -1,7 +1,7 @@
 import { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { login, logout } from '../../actions/userActions';
+import { login } from '../../actions/userActions';
 import { signIn } from '../../api/userAPI';
 
 import './AuthForm.css';
