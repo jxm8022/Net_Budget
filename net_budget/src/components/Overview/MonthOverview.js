c22e2b973450b981118dfbe1292a433fff6fe465
Jose Mendoza <jose.mendoza5@mavs.uta.edu>
2023-03-11 00:29:23 -0500

    Remove tool tip

--- net_budget/src/components/Overview/MonthOverview.js
+++ net_budget/src/components/Overview/MonthOverview.js
@@ -9,19 +9,17 @@ const MonthOverview = (props) => {
 
     return (
         <ul className='overview'>
-            <li className='tooltip'>
+            <li>
                 <h4>Potential Net</h4>
                 <p className={textClass(potNet)}>{'$' + potNet.toFixed(2)}</p>
-                <span className='tooltiptext tooltip-left arrow-right'>Potential Net is calculated by subtracting potential transactions from your current net. (Net + Potential Transactions)</span>
             </li>
             <li className='middle'>
                 <h4>Net</h4>
                 <p className={textClass(net)}>{'$' + net.toFixed(2)}</p>
             </li>
-            <li className='tooltip'>
+            <li>
                 <h4>Projected Net</h4>
                 <p className={textClass(projNet)}>{'$' + projNet.toFixed(2)}</p>
-                <span className='tooltiptext tooltip-right arrow-left'>Projected Net is calculated by adding your projected income to your potential net. (Potential Net + Projected Income)</span>
             </li>
         </ul >
     );
